f1002f98e4fe5733bd44f4abf6f6876e820e33c2
dan <dtmoodie@gmail.com>
2016-09-30 22:54:12 -0400

    On windows header only unit test doesn't link correctly.

--- tests/test_recompile/test_recompile.cpp
+++ tests/test_recompile/test_recompile.cpp
@@ -8,7 +8,7 @@
 
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE "MetaObject"
-#include <boost/test/included/unit_test.hpp>
+#include <boost/test/unit_test.hpp>
 #include <boost/filesystem.hpp>
 #include <iostream>
 
