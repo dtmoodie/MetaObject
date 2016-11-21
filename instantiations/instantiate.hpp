9e2a72b2b3dfc32dbe809cb665065ab54feb7903
dan <dtmoodie@gmail.com>
2016-11-20 23:06:39 -0500

    Export fix.

--- instantiations/instantiate.hpp
+++ instantiations/instantiate.hpp
@@ -1,9 +1,5 @@
 #pragma once
 
-#ifdef MO_EXPORTS
-  #undef MO_EXPORTS
-#endif
-
 #if (defined WIN32 || defined _WIN32 || defined WINCE || defined __CYGWIN__) && defined instantiations_EXPORTS
   #define INST_EXPORTS __declspec(dllexport)
 #elif defined __GNUC__ && __GNUC__ >= 4
