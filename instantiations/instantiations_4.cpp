7eea653fcd0d85b6a11ecb6ecc2f6197c6746f4e
dan <dtmoodie@gmail.com>
2016-12-27 01:49:41 -0500

    Split serialization stuff.

--- instantiations/instantiations_4.cpp
+++ instantiations/instantiations_4.cpp
@@ -7,35 +7,8 @@
 #include "MetaObject/Parameters/Buffers/StreamBuffer.hpp"
 #include "MetaObject/Parameters/Buffers/map.hpp"
 #include "MetaObject/Parameters/IO/CerealPolicy.hpp"
+#include "MetaObject/Parameters/IO/cvSpecializations.hpp"
 #include <boost/lexical_cast.hpp>
-
-namespace cereal
-{
-    template<class AR, class T> void serialize(AR& ar, cv::Point_<T>& pt)
-    {
-        ar(make_nvp("x", pt.x), make_nvp("y", pt.y));
-    }
-    template<class AR, class T> void serialize(AR& ar, cv::Point3_<T>& pt)
-    {
-        ar(make_nvp("x", pt.x), make_nvp("y", pt.y), make_nvp("z", pt.z));
-    }
-    template<class AR, class T> void serialize(AR& ar, cv::Scalar_<T>& pt)
-    {
-        ar(make_nvp("0", pt[0]), make_nvp("1", pt[1]), make_nvp("2", pt[2]), make_nvp("3", pt[3]));
-    }
-    template<class AR, class T> void serialize(AR& ar, cv::Rect_<T>& rect)
-    {
-        ar(make_nvp("x", rect.x), make_nvp("y", rect.y), make_nvp("width", rect.width), make_nvp("height", rect.height));
-    }
-
-    template<class AR, class T, int N> void serialize(AR& ar, cv::Vec<T, N>& vec)
-    {
-        for (int i = 0; i < N; ++i)
-        {
-            ar(make_nvp(std::to_string(i), vec[i]));
-        }
-    }
-}
 
 
 INSTANTIATE_META_PARAMETER(cv::Scalar);
---
+++ instantiations/instantiations_5.cpp
@@ -0,0 +1,19 @@
+#ifdef HAVE_OPENCV
+#include "MetaObject/Parameters/MetaParameter.hpp"
+#include "MetaObject/Parameters/UI/Qt/OpenCV.hpp"
+#include "MetaObject/Parameters/UI/Qt/Containers.hpp"
+#include "MetaObject/Parameters/UI/Qt/TParameterProxy.hpp"
+#include "MetaObject/Parameters/Buffers/CircularBuffer.hpp"
+#include "MetaObject/Parameters/Buffers/StreamBuffer.hpp"
+#include "MetaObject/Parameters/Buffers/map.hpp"
+#include "MetaObject/Parameters/IO/CerealPolicy.hpp"
+#include "MetaObject/Parameters/IO/cvSpecializations.hpp"
+#include "cereal/types/vector.hpp"
+#include <boost/lexical_cast.hpp>
+
+INSTANTIATE_META_PARAMETER(cv::Rect);
+INSTANTIATE_META_PARAMETER(cv::Rect2d);
+INSTANTIATE_META_PARAMETER(cv::Rect2f);
+INSTANTIATE_META_PARAMETER(std::vector<cv::Rect>);
+
+#endif
