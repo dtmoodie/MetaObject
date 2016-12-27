118f1346199d20bd188be39028f5b1458baeb87f
dan <dtmoodie@gmail.com>
2016-12-27 01:45:06 -0500

    Custom thread implementation for pooling and event queues.

---
+++ include/MetaObject/Thread/ThreadPool.hpp
@@ -0,0 +1,28 @@
+#pragma once
+#include <MetaObject/Detail/Export.hpp>
+#include "ThreadHandle.hpp"
+#include "Thread.hpp"
+namespace mo
+{
+    class Thread;
+    class MO_EXPORTS ThreadPool
+    {
+    public:
+        static ThreadPool* Instance();
+        ThreadHandle RequestThread();
+        void Cleanup();
+    protected:
+        friend class ThreadHandle;
+        void ReturnThread(Thread* thread);
+    private:
+        struct PooledThread
+        {
+            PooledThread(bool available_, Thread* thread_):
+                available(available_), thread(thread_){}
+            bool available = true;
+            int ref_count = 0;
+            Thread* thread;
+        };
+        std::list<PooledThread> _threads;
+    };
+}
---
+++ src/thread/Thread.cpp
@@ -0,0 +1,130 @@
+#include "MetaObject/Thread/Thread.hpp"
+#include "MetaObject/Signals/TypedSignalRelay.hpp"
+#include "MetaObject/Context.hpp"
+#include "MetaObject/Signals/TypedSlot.hpp"
+#include "MetaObject/Thread/BoostThread.h"
+#include "MetaObject/Thread/ThreadRegistry.hpp"
+using namespace mo;
+
+
+void Thread::PushEventQueue(const std::function<void(void)>& f)
+{
+    boost::mutex::scoped_lock lock(_mtx);
+    _event_queue.push(f);
+    _cv.notify_all();
+}
+// Work can be stolen and can exist on any thread
+void Thread::PushWork(const std::function<void(void)>& f)
+{
+    boost::mutex::scoped_lock lock(_mtx);
+    _work_queue.push(f);
+    _cv.notify_all();
+}
+void Thread::Start()
+{
+    _run = true;
+    _cv.notify_all();
+}
+void Thread::Stop()
+{
+    _run = false;
+}
+void Thread::SetExitCallback(const std::function<void(void)>& f)
+{
+    _on_exit = f;
+}
+void Thread::SetStartCallback(const std::function<void(void)>& f)
+{
+    _on_start = f;
+}
+std::shared_ptr<Connection> Thread::SetInnerLoop(TypedSlot<int(void)>* slot)
+{
+    return slot->Connect(_inner_loop);
+}
+ThreadPool* Thread::GetPool() const
+{
+    return _pool;
+}
+Context* Thread::GetContext() const
+{
+    return _ctx;
+}
+
+Thread::Thread()
+{
+    _pool = nullptr;
+    _ctx = nullptr;
+    _inner_loop.reset(new mo::TypedSignalRelay<int(void)>());
+    Stop();
+    _thread = boost::thread(&Thread::Main, this);
+}
+Thread::Thread(ThreadPool* pool)
+{
+    _inner_loop.reset(new mo::TypedSignalRelay<int(void)>());
+    _pool = pool;
+    _ctx = nullptr;
+    Stop();
+    _thread = boost::thread(&Thread::Main, this);
+}
+
+Thread::~Thread()
+{
+    _run = false;
+    _thread.interrupt();
+    _thread.join();
+}
+
+void Thread::Main()
+{
+    mo::Context ctx;
+    _ctx = &ctx;
+    mo::Context::SetDefaultThreadContext(_ctx);
+    if(_on_start)
+        _on_start();
+
+    while(!boost::this_thread::interruption_requested())
+    {
+        {
+            boost::mutex::scoped_lock lock(_mtx);
+            while (_work_queue.size())
+            {
+                _work_queue.back()();
+                _work_queue.pop();
+            }
+            while(_event_queue.size())
+            {
+                _event_queue.back()();
+                _event_queue.pop();
+            }
+        }
+        if(_run)
+        {
+            if (_inner_loop->HasSlots())
+            {
+                int delay = (*_inner_loop)();
+                if (delay)
+                {
+                    boost::this_thread::sleep_for(boost::chrono::milliseconds(delay));
+                }
+            }
+        }else
+        {
+            while(!_run)
+            {
+                boost::mutex::scoped_lock lock(_mtx);
+                _cv.wait_for(lock, boost::chrono::milliseconds(100));
+            }
+        }
+    }
+
+    if(_on_exit)
+        _on_exit();
+}
+size_t Thread::GetId() const
+{
+    return GetThreadId(_thread);
+}
+bool Thread::IsOnThread() const
+{
+    return GetId() == GetThisThread();
+}
