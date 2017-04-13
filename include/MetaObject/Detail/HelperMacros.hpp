8b458286417dc0a0fe5488c029cca31587109a04
dan <dtmoodie@gmail.com>
2017-04-13 17:32:59 -0400

    warning bug fixes

--- include/MetaObject/Detail/HelperMacros.hpp
+++ include/MetaObject/Detail/HelperMacros.hpp
@@ -5,13 +5,15 @@
 #define COMBINE1(X,Y) X##Y  // helper macro
 #define COMBINE(X,Y) COMBINE1(X,Y)
 
-#define MO_STRINGIFY_1(X1)                         #X1
-#define MO_STRINGIFY_2(X1, X2)                     #X1, #X2
-#define MO_STRINGIFY_3(X1, X2, X3)                 #X1, #X2, #X3
-#define MO_STRINGIFY_4(X1, X2, X3, X4)             #X1, #X2, #X3, #X4
-#define MO_STRINGIFY_5(X1, X2, X3, X4, X5)         #X1, #X2, #X3, #X4, #X5
-#define MO_STRINGIFY_6(X1, X2, X3, X4, X5, X6)     #X1, #X2, #X3, #X4, #X5, #X6
-#define MO_STRINGIFY_7(X1, X2, X3, X4, X5, X6, X7) #X1, #X2, #X3, #X4, #X5, #X6, #X7
+#define MO_STRINGIFY_1(X1)                                 #X1
+#define MO_STRINGIFY_2(X1, X2)                             #X1, #X2
+#define MO_STRINGIFY_3(X1, X2, X3)                         #X1, #X2, #X3
+#define MO_STRINGIFY_4(X1, X2, X3, X4)                     #X1, #X2, #X3, #X4
+#define MO_STRINGIFY_5(X1, X2, X3, X4, X5)                 #X1, #X2, #X3, #X4, #X5
+#define MO_STRINGIFY_6(X1, X2, X3, X4, X5, X6)             #X1, #X2, #X3, #X4, #X5, #X6
+#define MO_STRINGIFY_7(X1, X2, X3, X4, X5, X6, X7)         #X1, #X2, #X3, #X4, #X5, #X6, #X7
+#define MO_STRINGIFY_8(X1, X2, X3, X4, X5, X6, X7, X8)     #X1, #X2, #X3, #X4, #X5, #X6, #X7, #X8
+#define MO_STRINGIFY_9(X1, X2, X3, X4, X5, X6, X7, X8, X9) #X1, #X2, #X3, #X4, #X5, #X6, #X7, #X8, #X9
 
 #ifdef _MSC_VER
 #define MO_STRINGIFY(...)  BOOST_PP_CAT(BOOST_PP_OVERLOAD(MO_STRINGIFY_, __VA_ARGS__)(__VA_ARGS__), BOOST_PP_EMPTY())
