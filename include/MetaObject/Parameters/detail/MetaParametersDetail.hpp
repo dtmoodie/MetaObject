11a29055840723c77c5e0219e6cbece78fa88a9f
dan <dtmoodie@gmail.com>
2017-04-21 19:22:07 -0400

    template extern

--- include/MetaObject/Parameters/detail/MetaParametersDetail.hpp
+++ include/MetaObject/Parameters/detail/MetaParametersDetail.hpp
@@ -4,7 +4,15 @@
 #include "MetaObject/Parameters/detail/TypedInputParameterPtrImpl.hpp"
 #include "MetaObject/Parameters/detail/TypedParameterPtrImpl.hpp"
 
+#if (defined WIN32 || defined _WIN32 || defined WINCE || defined __CYGWIN__)
+  #define TEMPLATE_EXTERN extern
+#elif defined __GNUC__ && __GNUC__ >= 4
+  #define TEMPLATE_EXTERN
+#else
+  #define TEMPLATE_EXTERN
+#endif
+
 #define INSTANTIATE_META_PARAMETER(TYPE) \
 static mo::MetaParameter<TYPE, __COUNTER__> COMBINE(g_meta_parameter, __LINE__)(#TYPE); \
-template class mo::TypedInputParameterPtr<TYPE>; \
-template class mo::TypedParameterPtr<TYPE>;
+TEMPLATE_EXTERN template class mo::TypedInputParameterPtr<TYPE>; \
+TEMPLATE_EXTERN template class mo::TypedParameterPtr<TYPE>;
