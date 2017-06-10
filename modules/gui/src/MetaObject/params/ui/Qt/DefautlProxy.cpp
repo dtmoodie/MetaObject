2dfa904a61e43c287b77345130f9917cac6532c3
Dan <dtmoodie@gmail.com>
2017-06-10 17:58:43 -0400

    Check for target before adding to boost_targets use this-> when accessing member in DefaultProxy

--- modules/gui/src/MetaObject/params/ui/Qt/DefautlProxy.cpp
+++ modules/gui/src/MetaObject/params/ui/Qt/DefautlProxy.cpp
@@ -9,20 +9,20 @@ DefaultProxy::DefaultProxy(IParam* param)
 {
     delete_slot = std::bind(&DefaultProxy::onParamDelete, this, std::placeholders::_1);
     update_slot = std::bind(&DefaultProxy::onParamUpdate, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3, std::placeholders::_4, std::placeholders::_5, std::placeholders::_6);
-    param = param;
+    this->param = param;
     param->registerUpdateNotifier(&update_slot);
     param->registerDeleteNotifier(&delete_slot);
 }
 bool DefaultProxy::setParam(IParam* param)
 {
-    param = param;
+    this->param = param;
     param->registerUpdateNotifier(&update_slot);
     param->registerDeleteNotifier(&delete_slot);
     return true;
 }
 bool DefaultProxy::checkParam(IParam* param) const
 {    
-    return param == param;
+    return this->param == param;
 }
 #ifdef HAVE_QT5
 #include <qgridlayout.h>
@@ -40,12 +40,14 @@ QWidget* DefaultProxy::getParamWidget(QWidget* parent)
     layout->addWidget(nameLbl, 0, 0);
     output->setLayout(layout);
     return output;
-#endif
+#else
 	return nullptr;
+#endif
 }
 
 void DefaultProxy::onUiUpdate(QObject* source)
 {
+    (void)source;
 }
 
 void DefaultProxy::onParamUpdate(IParam*, Context*, OptionalTime_t, size_t, ICoordinateSystem*, UpdateFlags)
