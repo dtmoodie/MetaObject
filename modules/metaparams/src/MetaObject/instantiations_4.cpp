5ded6c16b16556a14f8fba7761d46d6be1d58f30
dan <dtmoodie@gmail.com>
2017-07-31 06:10:01 -0400

    text serialization

--- modules/metaparams/src/MetaObject/instantiations_4.cpp
+++ modules/metaparams/src/MetaObject/instantiations_4.cpp
@@ -1,16 +1,16 @@
 #ifdef HAVE_OPENCV
 #include "MetaObject/params/MetaParam.hpp"
 #ifdef HAVE_QT
-#include "MetaObject/params/ui/Qt/OpenCV.hpp"
 #include "MetaObject/params/ui/Qt/Containers.hpp"
+#include "MetaObject/params/ui/Qt/OpenCV.hpp"
 #include "MetaObject/params/ui/Qt/TParamProxy.hpp"
 #endif
 #include "MetaObject/params/buffers/CircularBuffer.hpp"
-#include "MetaObject/params/buffers/StreamBuffer.hpp"
 #include "MetaObject/params/buffers/Map.hpp"
+#include "MetaObject/params/buffers/StreamBuffer.hpp"
 #include "MetaObject/serialization/CerealPolicy.hpp"
-#include "MetaObject/serialization/cvSpecializations.hpp"
 #include "MetaObject/serialization/cereal_map.hpp"
+#include "MetaObject/serialization/cvSpecializations.hpp"
 #include "cereal/types/map.hpp"
 #include "cereal/types/string.hpp"
 #include <cereal/types/vector.hpp>
@@ -19,41 +19,45 @@
 #undef MO_EXPORTS
 #endif
 #if (defined WIN32 || defined _WIN32 || defined WINCE || defined __CYGWIN__)
-#  define MO_EXPORTS __declspec(dllexport)
+#define MO_EXPORTS __declspec(dllexport)
 #elif defined __GNUC__ && __GNUC__ >= 4
-#  define MO_EXPORTS __attribute__ ((visibility ("default")))
+#define MO_EXPORTS __attribute__((visibility("default")))
 #else
-#  define MO_EXPORTS
+#define MO_EXPORTS
 #endif
 #include "MetaObject/params/detail/MetaParamImpl.hpp"
 #include <boost/lexical_cast.hpp>
 
-namespace mo
-{
-namespace IO
-{
-namespace Text
-{
-    namespace imp
-    {
-        void Serialize_imp(std::ostream &os, const cv::Scalar& obj, int)
-        {
-            os << obj.val[0] << ", " << obj.val[1] << ", " <<  obj.val[2] << ", " << obj.val[3];
-        }
-        void DeSerialize_imp(std::istream &is, cv::Scalar& obj, int)
-        {
-            char c;
-            for(int i = 0; i < 4; ++i)
-            {
-                is >> obj[i];
-                is >> c;
+namespace mo {
+namespace IO {
+    namespace Text {
+        namespace imp {
+            void Serialize_imp(std::ostream& os, const cv::Scalar& obj, int) {
+                os << obj.val[0] << ", " << obj.val[1] << ", " << obj.val[2] << ", " << obj.val[3];
+            }
+            void DeSerialize_imp(std::istream& is, cv::Scalar& obj, int) {
+                char c;
+                for (int i = 0; i < 4; ++i) {
+                    is >> obj[i];
+                    is >> c;
+                }
             }
         }
     }
 }
 }
-}
+
 #include "MetaObject/serialization/TextPolicy.hpp"
+#define ASSERT_SERIALIZABLE(TYPE) static_assert(mo::IO::Text::imp::stream_serializable<TYPE>::value, "Checking stream serializable for " #TYPE)
+
+namespace cv {
+template <class T>
+std::ostream& operator<<(std::ostream& os, const cv::Scalar_<T>& obj) {
+    ASSERT_SERIALIZABLE(cv::Scalar);
+    os << obj.val[0] << ", " << obj.val[1] << ", " << obj.val[2] << ", " << obj.val[3];
+    return os;
+}
+}
 INSTANTIATE_META_PARAM(cv::Scalar);
 INSTANTIATE_META_PARAM(cv::Vec2f);
 INSTANTIATE_META_PARAM(cv::Vec3f);
