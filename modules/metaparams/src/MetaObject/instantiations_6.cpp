d21ee8060c4a6eccf293a03a98623555854a066d
dan <dtmoodie@gmail.com>
2017-08-04 15:23:09 -0400

    serialization fix

--- modules/metaparams/src/MetaObject/instantiations_6.cpp
+++ modules/metaparams/src/MetaObject/instantiations_6.cpp
@@ -1,30 +1,8 @@
 #include <MetaObject/params/MetaParam.hpp>
-#ifdef HAVE_QT
-#include "MetaObject/params/ui/Qt/OpenCV.hpp"
-#include "MetaObject/params/ui/Qt/Containers.hpp"
-#include "MetaObject/params/ui/Qt/TParamProxy.hpp"
-#endif
-#include "MetaObject/params/buffers/CircularBuffer.hpp"
-#include "MetaObject/params/buffers/StreamBuffer.hpp"
-#include "MetaObject/params/buffers/Map.hpp"
-#include "MetaObject/serialization/CerealPolicy.hpp"
-#include "MetaObject/serialization/TextPolicy.hpp"
+#include <MetaObject/params/ITAccessibleParam.hpp>
 #include "MetaObject/params/Types.hpp"
-#ifdef MO_EXPORTS
-#undef MO_EXPORTS
-#endif
-#if (defined WIN32 || defined _WIN32 || defined WINCE || defined __CYGWIN__)
-#  define MO_EXPORTS __declspec(dllexport)
-#elif defined __GNUC__ && __GNUC__ >= 4
-#  define MO_EXPORTS __attribute__ ((visibility ("default")))
-#else
-#  define MO_EXPORTS
-#endif
-#include "MetaObject/params/detail/MetaParamImpl.hpp"
-
-#include <cereal/types/vector.hpp>
-#include <cereal/types/string.hpp>
-
+#include <MetaObject/params/AccessToken.hpp>
+#include <boost/lexical_cast.hpp>
 namespace mo{
 namespace IO{
 namespace Text{
@@ -68,6 +46,47 @@ bool Serialize(ITAccessibleParam<EnumParam>* param, std::stringstream& ss){
 } // namespace Text
 } // namespace IO
 } // namespace mo
+
+#ifdef HAVE_QT
+#include "MetaObject/params/ui/Qt/OpenCV.hpp"
+#include "MetaObject/params/ui/Qt/Containers.hpp"
+#include "MetaObject/params/ui/Qt/TParamProxy.hpp"
+#endif
+#include "MetaObject/params/buffers/CircularBuffer.hpp"
+#include "MetaObject/params/buffers/StreamBuffer.hpp"
+#include "MetaObject/params/buffers/Map.hpp"
+#include "MetaObject/serialization/CerealPolicy.hpp"
+#include "MetaObject/serialization/TextPolicy.hpp"
+
+#ifdef MO_EXPORTS
+#undef MO_EXPORTS
+#endif
+#if (defined WIN32 || defined _WIN32 || defined WINCE || defined __CYGWIN__)
+#  define MO_EXPORTS __declspec(dllexport)
+#elif defined __GNUC__ && __GNUC__ >= 4
+#  define MO_EXPORTS __attribute__ ((visibility ("default")))
+#else
+#  define MO_EXPORTS
+#endif
+
+#include "MetaObject/params/detail/MetaParamImpl.hpp"
+
+#include <cereal/types/vector.hpp>
+#include <cereal/types/string.hpp>
+
+namespace mo {
+std::ostream& operator<<(std::ostream& os, const mo::EnumParam& obj) {
+    ASSERT_SERIALIZABLE(EnumParam);
+    for(size_t i = 0; i < obj.enumerations.size(); ++i){
+        if(i == obj.currentSelection){
+            os << '>' << obj.enumerations[i] << "<, ";
+        }else{
+            os << obj.enumerations[i] << ", ";
+        }
+    }
+    return os;
+}
+}
 
 using namespace mo;
 template<class AR> void EnumParam::serialize(AR& ar){
