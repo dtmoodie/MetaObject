b403fb14c89997abe266804cf40d7b69379681ab
dan <dtmoodie@gmail.com>
2017-08-04 15:20:49 -0400

    return a TypeInfo from type name

--- modules/core/src/MetaObject/core/Demangle.hpp
+++ modules/core/src/MetaObject/core/Demangle.hpp
@@ -9,6 +9,7 @@ class TypeInfo;
 class MO_EXPORTS Demangle {
 public:
     static std::string typeToName(const TypeInfo& type);
+    static const TypeInfo& nameToType(const std::string& name);
     static void registerName(const TypeInfo& type, const char* name);
     static void registerType(const TypeInfo& type);
     // This returns a type map that is stored in cereal binary format that
