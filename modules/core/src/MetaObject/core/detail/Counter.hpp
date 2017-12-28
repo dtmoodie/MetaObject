bf02a25939e0bbffc2c8d7f83504a8e6356a0625
dan <dtmoodie@gmail.com>
2017-12-28 17:25:17 -0500

    updated formatting

--- modules/core/src/MetaObject/core/detail/Counter.hpp
+++ modules/core/src/MetaObject/core/detail/Counter.hpp
@@ -1,12 +1,18 @@
 #pragma once
 
-namespace mo {
-template<int N> struct _counter_ {
-    _counter_<N-1> operator--() {
-        return _counter_<N-1>();
-    }
-    _counter_<N+1> operator++() {
-        return _counter_<N+1>();
-    }
-};
+namespace mo
+{
+    template<int N>
+    struct _counter_
+    {
+        _counter_<N-1> operator--()
+        {
+            return _counter_<N-1>();
+        }
+
+        _counter_<N+1> operator++()
+        {
+            return _counter_<N+1>();
+        }
+    };
 }
