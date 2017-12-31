501102b7592c6497fdf157a24556a376ecfd374d
dan <dtmoodie@gmail.com>
2017-12-31 11:30:28 -0500

    inheritance of reflection data

--- modules/params/src/MetaObject/params/detail/reflect_data.hpp
+++ modules/params/src/MetaObject/params/detail/reflect_data.hpp
@@ -7,66 +7,83 @@ namespace mo
     namespace reflect
     {
 
-        template<class T, class Enable = void>
+        template <class T, class Enable = void>
         struct ReflectData;
 
-        template<int I, class T>
-        static constexpr inline auto get(T& data) -> decltype(ReflectData<std::remove_const_t<T>>::get(data, mo::_counter_<I>()));
+        template <int I, class T>
+        static constexpr inline auto get(T& data)
+            -> decltype(ReflectData<std::remove_const_t<T>>::get(data, mo::_counter_<I>()));
 
-        template<int I, class T>
-        static constexpr inline auto get(const T& data) -> decltype(ReflectData<std::remove_const_t<T>>::get(data, mo::_counter_<I>()));
+        template <int I, class T>
+        static constexpr inline auto get(const T& data)
+            -> decltype(ReflectData<std::remove_const_t<T>>::get(data, mo::_counter_<I>()));
 
-        template<int I, class T>
-        static constexpr inline auto getValue(const T& data){return get<I, T>(data);}
+        template <int I, class T>
+        static constexpr inline auto getValue(const T& data)
+        {
+            return get<I, T>(data);
+        }
 
-        template<int I, class T>
-        static constexpr inline void setValue(T& data, const decltype(get<I, T>(data))& value) { get<I, T>(data) = value; }
+        template <int I, class T>
+        static constexpr inline void setValue(T& data, const decltype(get<I, T>(data))& value)
+        {
+            get<I, T>(data) = value;
+        }
 
-        template<int I, class T>
+        template <int I, class T>
         static constexpr inline const char* getName();
 
-        template<class T>
+        template <class T>
         struct ReflectData<T, decltype(T::get(std::declval<T>(), mo::_counter_<0>()), void())>
         {
             static constexpr bool IS_SPECIALIZED = true;
             static constexpr int N = T::N;
-            static constexpr auto get(const T& data, mo::_counter_<0>){return T::get(data, mo::_counter_<0>());}
-            static constexpr auto get(T& data, mo::_counter_<0>){return T::get(data, mo::_counter_<0>());}
-            template<int I>
-            static constexpr auto get(const T& data, mo::_counter_<I>){return T::get(data, mo::_counter_<I>());}
-            template<int I>
-            static constexpr auto get(T& data, mo::_counter_<I>){return T::get(data, mo::_counter_<I>());}
-            static constexpr const char* getName(mo::_counter_<0> cnt){return T::getName(cnt);}
-            template<int I>
-            static constexpr const char* getName(mo::_counter_<I> cnt){return T::getName(cnt);}
+            static constexpr auto get(const T& data, mo::_counter_<0>) { return T::get(data, mo::_counter_<0>()); }
+            static constexpr auto get(T& data, mo::_counter_<0>) { return T::get(data, mo::_counter_<0>()); }
+            template <int I>
+            static constexpr auto get(const T& data, mo::_counter_<I>)
+            {
+                return T::get(data, mo::_counter_<I>());
+            }
+            template <int I>
+            static constexpr auto get(T& data, mo::_counter_<I>)
+            {
+                return T::get(data, mo::_counter_<I>());
+            }
+            static constexpr const char* getName(mo::_counter_<0> cnt) { return T::getName(cnt); }
+            template <int I>
+            static constexpr const char* getName(mo::_counter_<I> cnt)
+            {
+                return T::getName(cnt);
+            }
         };
 
         namespace detail
         {
-            template<class T>
+            template <class T>
             static constexpr inline void printStructHelper(std::ostream& os, const T& data, mo::_counter_<0>)
             {
                 os << getName<0, T>() << ':' << get<0>(data);
             }
 
-            template<int I, class T>
+            template <int I, class T>
             static constexpr inline void printStructHelper(std::ostream& os, const T& data, mo::_counter_<I>)
             {
-                printStructHelper(os, data, mo::_counter_<I-1>());
+                printStructHelper(os, data, mo::_counter_<I - 1>());
                 os << ' ' << getName<I, T>() << ':' << get<I>(data);
             }
 
-            template<class AR, class T>
+            template <class AR, class T>
             static constexpr inline void serializeHelper(AR& ar, T& data, mo::_counter_<0>)
             {
                 ar(cereal::make_nvp(getName<0, T>(), get<0>(data)));
             }
 
-            template<int I, class AR, class T>
+            template <int I, class AR, class T>
             static constexpr inline void serializeHelper(AR& ar, T& data, mo::_counter_<I>)
             {
+                serializeHelper(ar, data, mo::_counter_<I - 1>());
                 ar(cereal::make_nvp(getName<I, T>(), get<I>(data)));
-                serializeHelper(ar, data, mo::_counter_<I-1>());
             }
         }
     }
--- modules/params/tests/reflect_pod.cpp
+++ modules/params/tests/reflect_pod.cpp
@@ -1,15 +1,21 @@
 #include "MetaObject/params/reflect_data.hpp"
-#include <iostream>
 #include <cereal/archives/json.hpp>
+#include <iostream>
 
 using namespace mo;
 
 struct ReflectedStruct
 {
+  public:
     float x;
     float y;
     float z;
     int id;
+};
+
+struct Inherited : public ReflectedStruct
+{
+    float w;
 };
 
 namespace mo
@@ -17,23 +23,31 @@ namespace mo
     namespace reflect
     {
         REFLECT_DATA_START(ReflectedStruct);
-            REFLECT_DATA_MEMBER(x)
-            REFLECT_DATA_MEMBER(y)
-            REFLECT_DATA_MEMBER(z)
-            REFLECT_DATA_MEMBER(id)
+        REFLECT_DATA_MEMBER(x)
+        REFLECT_DATA_MEMBER(y)
+        REFLECT_DATA_MEMBER(z)
+        REFLECT_DATA_MEMBER(id)
         REFLECT_DATA_END();
     }
 }
 
+namespace mo
+{
+    namespace reflect
+    {
+        REFLECT_DATA_DERIVED(Inherited, ReflectedStruct);
+        REFLECT_DATA_MEMBER(w)
+        REFLECT_DATA_END();
+    }
+}
 
 REFLECT_INTERNAL_START(InternallyReflected)
-    REFLECT_INTERNAL_MEMBER(float, x)
-    REFLECT_INTERNAL_MEMBER(float, y)
-    REFLECT_INTERNAL_MEMBER(float, z)
+REFLECT_INTERNAL_MEMBER(float, x)
+REFLECT_INTERNAL_MEMBER(float, y)
+REFLECT_INTERNAL_MEMBER(float, z)
 REFLECT_INTERNAL_END();
 
-
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
     InternallyReflected data2;
     ReflectedStruct data;
@@ -42,15 +56,34 @@ int main(int argc, char** argv)
     data.z = 2;
     data.id = 3;
     mo::reflect::printStruct(std::cout, data);
-    static_assert(std::is_same<mo::reflect::enable_if_reflected<ReflectedStruct>, void>::value, "test1");
-    cereal::JSONOutputArchive ar(std::cout);
-    mo::reflect::serialize(ar, data);
-    static_assert(std::is_same<mo::reflect::enable_if_reflected<InternallyReflected>, void>::value, "test2");
+    std::cout << std::endl;
+    static_assert(
+        std::is_same<mo::reflect::enable_if_reflected<ReflectedStruct>,
+                     void>::value,
+        "test1");
+    {
+        cereal::JSONOutputArchive ar(std::cout);
+        mo::reflect::serialize(ar, data);
+    }
     std::cout << std::endl;
     data2.x = 5;
     data2.y = 6;
     data2.z = 10;
     mo::reflect::printStruct(std::cout, data2);
+    std::cout << std::endl;
+    static_assert(mo::reflect::ReflectData<Inherited>::I0 == 4,
+                  "Fetching base param count is broken");
+    static_assert(mo::reflect::ReflectData<Inherited>::N == 5,
+                  "Inheritance param counting is broken");
+    std::cout << std::endl;
+
+    Inherited test;
+    mo::reflect::printStruct(std::cout, test);
+    std::cout << std::endl;
+    {
+        cereal::JSONOutputArchive ar(std::cout);
+        mo::reflect::serialize(ar, test);
+    }
     std::cout << std::endl;
     return 0;
 }
