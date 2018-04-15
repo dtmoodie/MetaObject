7ff8492ba02f01f53acd609874a866e836afbc8c
dan <dtmoodie@gmail.com>
2018-04-15 17:59:50 -0400

    fixed bugs wrt multi input

--- modules/params/src/MetaObject/params/TMultiInput.hpp
+++ modules/params/src/MetaObject/params/TMultiInput.hpp
@@ -1,24 +1,57 @@
 #pragma once
 
+#include "InputParamAny.hpp"
 #include "TInputParam.hpp"
 #include "TypeSelector.hpp"
 
 namespace mo
 {
+    template <class T, class U>
+    constexpr int indexOfHelper(int idx, std::tuple<U>* = nullptr)
+    {
+        return idx;
+    }
+    template <class T, class U, class... Ts>
+    constexpr int indexOfHelper(int idx, std::tuple<U, Ts...>* = nullptr)
+    {
+        return std::is_same<T, U>::value ? idx
+                                         : indexOfHelper<T, Ts...>(idx - 1, static_cast<std::tuple<Ts...>*>(nullptr));
+    }
+
+    template <class T, class... Ts>
+    constexpr int indexOf()
+    {
+        return sizeof...(Ts)-indexOfHelper<T, Ts...>(sizeof...(Ts), static_cast<std::tuple<Ts...>*>(nullptr));
+    }
+
+    template <class T, class... Ts>
+    T& get(std::tuple<Ts...>& tuple)
+    {
+        return std::get<indexOf<T, Ts...>()>(tuple);
+    }
+
+    template <class T, class... Ts>
+    const T& get(const std::tuple<Ts...>& tuple)
+    {
+        static const int IDX = indexOf<T, Ts...>();
+        return std::get<IDX>(tuple);
+    }
+
     template <class... Types>
-    class TMultiInput : public InputParam
+    class TMultiInput : public InputParamAny
     {
       public:
-        void setUserDataPtr(std::tuple<Types*...>* user_var_) { typeLoop(*this, user_var_); }
+        void setUserDataPtr(std::tuple<const Types*...>* user_var_) { typeLoop<Types...>(*this, user_var_); }
 
         virtual bool setInput(std::shared_ptr<IParam> input) override
         {
+            bool success = false;
             if (m_current_input)
             {
-                selectType(*this, m_current_input->getTypeInfo(), static_cast<IParam*>(nullptr));
+                selectType<Types...>(*this, m_current_input->getTypeInfo(), static_cast<IParam*>(nullptr), &success);
+                success = false;
             }
-            bool success = false;
-            selectType(*this, input->getTypeInfo(), &success);
+            selectType<Types...>(*this, input->getTypeInfo(), input.get(), &success);
             if (success)
             {
                 m_current_input = input.get();
@@ -28,12 +61,13 @@ namespace mo
 
         virtual bool setInput(IParam* input) override
         {
+            bool success = false;
             if (m_current_input)
             {
-                selectType(*this, m_current_input->getTypeInfo(), static_cast<IParam*>(nullptr));
+                selectType<Types...>(*this, m_current_input->getTypeInfo(), static_cast<IParam*>(nullptr), &success);
+                success = false;
             }
-            bool success = false;
-            selectType(*this, input->getTypeInfo(), &success);
+            selectType<Types...>(*this, input->getTypeInfo(), input, &success);
             if (success)
             {
                 m_current_input = input;
@@ -46,7 +80,7 @@ namespace mo
             bool success = false;
             if (m_current_input)
             {
-                selectType(*this, m_current_input->getTypeInfo(), ts, fn, &success);
+                selectType<Types...>(*this, m_current_input->getTypeInfo(), ts, fn, &success);
             }
             return success;
         }
@@ -56,53 +90,65 @@ namespace mo
             bool success = false;
             if (m_current_input)
             {
-                selectType(*this, m_current_input->getTypeInfo(), fn, ts, &success);
+                selectType<Types...>(*this, m_current_input->getTypeInfo(), fn, ts, &success);
             }
             return success;
         }
 
         virtual void setMtx(Mutex_t* mtx) override { typeLoop<Types...>(*this, mtx); }
 
+        const mo::TypeInfo& getTypeInfo() const
+        {
+            if (m_current_input == nullptr)
+            {
+                return InputParamAny::getTypeInfo();
+            }
+            else
+            {
+                return m_current_input->getTypeInfo();
+            }
+        }
+
         template <class T>
         void apply(std::shared_ptr<IParam> input, bool* success)
         {
-            *success = std::get<TInputParamPtr<T>>(m_inputs).setInput(input);
+            *success = get<TInputParamPtr<T>>(m_inputs).setInput(input);
         }
 
         template <class T>
         void apply(IParam* input, bool* success)
         {
-            *success = std::get<TInputParamPtr<T>>(m_inputs).setInput(input);
+            *success = get<TInputParamPtr<T>>(m_inputs).setInput(input);
         }
 
         template <class T>
-        void apply(std::tuple<Types*...>* user_var_)
+        void apply(std::tuple<const Types*...>* user_var_)
         {
-            std::get<TInputParamPtr<T>>(m_inputs).setUserDataPtr(&std::get<T*>(*user_var_));
+            get<TInputParamPtr<T>>(m_inputs).setUserDataPtr(&get<const T*>(*user_var_));
         }
 
         template <class T>
         void apply(Mutex_t* mtx)
         {
-            std::get<TInputParamPtr<T>>(m_inputs).setMtx(mtx);
+            get<TInputParamPtr<T>>(m_inputs).setMtx(mtx);
         }
 
         template <class T>
         void apply(Context* ctx)
         {
-            std::get<TInputParamPtr<T>>(m_inputs).setContext(ctx);
+            get<TInputParamPtr<T>>(m_inputs).setContext(ctx);
         }
 
         template <class T>
         void apply(const OptionalTime_t& ts, size_t* fn, bool* success)
         {
-            *success = std::get<TInputParamPtr<T>>(m_inputs).getInput(ts, fn);
+            *success = get<TInputParamPtr<T>>(m_inputs).getInput(ts, fn);
         }
 
         template <class T>
         void apply(size_t fn, OptionalTime_t* ts, bool* success)
         {
-            *success = std::get<TInputParamPtr<T>>(m_inputs).getInput(fn, ts);
+            *success = get<TInputParamPtr<T>>(m_inputs).getInput(fn, ts);
         }
 
       private:
--- modules/params/src/MetaObject/params/TypeSelector.hpp
+++ modules/params/src/MetaObject/params/TypeSelector.hpp
@@ -1,26 +1,27 @@
 #pragma once
+#include <MetaObject/core/detail/Counter.hpp>
 #include <MetaObject/detail/TypeInfo.hpp>
 #include <tuple>
 
 namespace mo
 {
-    template <class F, class T>
-    struct TypeSelector;
+    /*template <class F, class T>
+    struct TypeSelector;*/
 
     template <class F, class... T>
-    struct TypeSelector<F, T...>
+    struct TypeSelector
     {
         TypeSelector(F& functor) : m_func(functor) {}
 
         template <class... Args>
         bool apply(const mo::TypeInfo& type, Args&&... args)
         {
-            return applyImpl(std::integral_constant<int, sizeof...(T)-1>(), type, std::forward<Args>(args)...);
+            return applyImpl(_counter_<static_cast<int>(sizeof...(T)) - 1>(), type, std::forward<Args>(args)...);
         }
 
       private:
         template <class... Args>
-        bool applyImpl(std::integral_constant<int, 0>, const mo::TypeInfo& type, Args&&... args)
+        bool applyImpl(_counter_<0>, const mo::TypeInfo& type, Args&&... args)
         {
             typedef typename std::tuple_element<0, std::tuple<T...>>::type Type;
             if (mo::TypeInfo(typeid(Type)) == type)
@@ -31,7 +32,8 @@ namespace mo
         }
 
         template <int I, class... Args>
-        bool applyImpl(std::integral_constant<int, I>, const mo::TypeInfo& type, Args&&... args)
+        bool
+        applyImpl(_counter_<I> cnt, const typename std::enable_if<I != 0, mo::TypeInfo>::type& type, Args&&... args)
         {
             typedef typename std::tuple_element<I, std::tuple<T...>>::type Type;
             if (mo::TypeInfo(typeid(Type)) == type)
@@ -40,18 +42,15 @@ namespace mo
             }
             else
             {
-                return applyImpl(std::integral_constant<int, I - 1>(), type, std::forward<Args>(args)...);
+                return applyImpl(--cnt, type, std::forward<Args>(args)...);
             }
         }
 
         F& m_func;
     };
 
-    template <class F, class T>
-    struct TypeLoop;
-
     template <class F, class... T>
-    struct TypeLoop<F, T...>
+    struct TypeLoop
     {
         TypeLoop(F& func) : m_func(func) {}
         template <class... Args>
@@ -80,6 +79,7 @@ namespace mo
     template <class... Types, class F, class... Args>
     bool selectType(F& func, const mo::TypeInfo& type, Args&&... args)
     {
+        static_assert(sizeof...(Types) > 0, "Must have multiple type inputs");
         TypeSelector<F, Types...> selector(func);
         return selector.apply(type, std::forward<Args>(args)...);
     }
