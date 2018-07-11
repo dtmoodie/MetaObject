5f915eb4baf359028a0897052fd0d6ab66368c43
dan <dtmoodie@gmail.com>
2018-07-11 19:41:47 -0400

    clang format

--- modules/params/src/MetaObject/params/detail/print_data.hpp
+++ modules/params/src/MetaObject/params/detail/print_data.hpp
@@ -1,16 +1,18 @@
 #pragma once
+#include <MetaObject/core/Demangle.hpp>
+#include <MetaObject/core/metaobject_config.hpp>
 
 #include "ct/reflect/printer.hpp"
 #include "ct/reflect/reflect_data.hpp"
 #include <ct/detail/TypeTraits.hpp>
-#include <MetaObject/core/Demangle.hpp>
 
 namespace mo
 {
     namespace detail
     {
         template <class T>
-        struct stream_serializable {
+        struct stream_serializable
+        {
             template <class U>
             static constexpr auto check(std::ostream* os, U* val) -> decltype(*os << *val, size_t(0))
             {
@@ -22,23 +24,26 @@ namespace mo
             {
                 return 0;
             }
-            static const bool value = sizeof(check<T>(static_cast<std::ostream*>(nullptr), static_cast<T*>(nullptr))) == sizeof(size_t);
+            static const bool value =
+                sizeof(check<T>(static_cast<std::ostream*>(nullptr), static_cast<T*>(nullptr))) == sizeof(size_t);
         };
     } // detail::
 
-    template<class T>
-    auto printImpl(std::ostream& os, const T& data) -> typename std::enable_if<detail::stream_serializable<T>::value>::type
+    template <class T>
+    auto printImpl(std::ostream& os, const T& data) ->
+        typename std::enable_if<detail::stream_serializable<T>::value>::type
     {
         os << data;
     }
 
-    template<class T>
-    auto printImpl(std::ostream& os, const T& data) -> typename std::enable_if<!detail::stream_serializable<T>::value>::type
+    template <class T>
+    auto printImpl(std::ostream& os, const T & /*data*/) ->
+        typename std::enable_if<!detail::stream_serializable<T>::value>::type
     {
         os << "No stringifier available for " << mo::Demangle::typeToName(mo::TypeInfo(typeid(T))) << std::endl;
     }
 
-    template<class T>
+    template <class T>
     void print(std::ostream& os, const T& data)
     {
         printImpl(os, data);
