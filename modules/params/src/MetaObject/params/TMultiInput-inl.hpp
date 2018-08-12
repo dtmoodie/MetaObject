3cf63db13d8e1865b1d5ecefa16434f9aee3eb11
dan <dtmoodie@gmail.com>
2018-08-12 14:58:47 -0400

    fixed emission of signals

--- modules/params/src/MetaObject/params/TMultiInput-inl.hpp
+++ modules/params/src/MetaObject/params/TMultiInput-inl.hpp
@@ -29,13 +29,14 @@ namespace mo
     template <class... Types>
     bool TMultiInput<Types...>::setInput(std::shared_ptr<IParam> input)
     {
+        mo::Mutex_t::scoped_lock lock(mtx());
         bool success = false;
         if (m_current_input)
         {
             selectType<Types...>(*this, m_current_input->getTypeInfo(), static_cast<IParam*>(nullptr), &success);
             success = false;
         }
-        selectType<Types...>(*this, input->getTypeInfo(), input.get(), &success);
+        selectType<Types...>(*this, input->getTypeInfo(), input, &success);
         if (success)
         {
             m_current_input = input.get();
@@ -46,6 +47,7 @@ namespace mo
     template <class... Types>
     bool TMultiInput<Types...>::setInput(IParam* input)
     {
+        mo::Mutex_t::scoped_lock lock(mtx());
         bool success = false;
         if (m_current_input)
         {
@@ -63,10 +65,13 @@ namespace mo
     template <class... Types>
     bool TMultiInput<Types...>::getInput(const OptionalTime_t& ts, size_t* fn)
     {
+        mo::Mutex_t::scoped_lock lock(mtx());
         bool success = false;
         if (m_current_input)
         {
             selectType<Types...>(*this, m_current_input->getTypeInfo(), ts, fn, &success);
+            this->_ts = m_current_input->getTimestamp();
+            this->_fn = m_current_input->getFrameNumber();
         }
         return success;
     }
@@ -74,6 +79,7 @@ namespace mo
     template <class... Types>
     bool TMultiInput<Types...>::getInput(size_t fn, OptionalTime_t* ts)
     {
+        mo::Mutex_t::scoped_lock lock(mtx());
         bool success = false;
         if (m_current_input)
         {
@@ -88,12 +94,14 @@ namespace mo
     template <class... Types>
     void TMultiInput<Types...>::setMtx(Mutex_t* mtx)
     {
+        InputParam::setMtx(mtx);
         typeLoop<Types...>(*this, mtx);
     }
 
     template <class... Types>
     mo::TypeInfo TMultiInput<Types...>::getTypeInfo() const
     {
+        mo::Mutex_t::scoped_lock lock(mtx());
         if (m_current_input == nullptr)
         {
             return _void_type_info;
@@ -107,20 +115,25 @@ namespace mo
     template <class... Types>
     mo::IParam* TMultiInput<Types...>::getInputParam() const
     {
+        mo::Mutex_t::scoped_lock lock(mtx());
         return m_current_input;
     }
 
     template <class... Types>
     OptionalTime_t TMultiInput<Types...>::getInputTimestamp()
     {
+        mo::Mutex_t::scoped_lock lock(mtx());
         if (m_current_input)
+        {
             return m_current_input->getTimestamp();
+        }
         return {};
     }
 
     template <class... Types>
     size_t TMultiInput<Types...>::getInputFrameNumber()
     {
+        mo::Mutex_t::scoped_lock lock(mtx());
         if (m_current_input)
         {
             return m_current_input->getFrameNumber();
@@ -131,12 +144,14 @@ namespace mo
     template <class... Types>
     bool TMultiInput<Types...>::isInputSet() const
     {
+        mo::Mutex_t::scoped_lock lock(mtx());
         return m_current_input != nullptr;
     }
 
     template <class... Types>
     bool TMultiInput<Types...>::acceptsInput(IParam* input) const
     {
+        mo::Mutex_t::scoped_lock lock(mtx());
         AcceptInputRedirect<TMultiInput<Types...>> redirect(*this);
         bool success = false;
         selectType<Types...>(redirect, input->getTypeInfo(), input, &success);
@@ -146,6 +161,7 @@ namespace mo
     template <class... Types>
     bool TMultiInput<Types...>::acceptsType(const TypeInfo& type) const
     {
+        mo::Mutex_t::scoped_lock lock(mtx());
         AcceptInputRedirect<TMultiInput<Types...>> redirect(*this);
         bool success = false;
         selectType<Types...>(redirect, type, type, &success);
@@ -213,6 +229,79 @@ namespace mo
     void TMultiInput<Types...>::acceptsInput(const TypeInfo& type, bool* success) const
     {
         *success = mo::get<TInputParamPtr<T>>(m_inputs).acceptsType(type);
+    }
+
+    template <class... Types>
+    template<class T, class Slot>
+    void TMultiInput<Types...>::apply(std::vector<std::shared_ptr<Connection>>& connections, Slot slot)
+    {
+        connections.push_back(mo::get<TInputParamPtr<T>>(m_inputs).registerUpdateNotifier(slot));
+    }
+
+    template <class... Types>
+    std::shared_ptr<Connection> TMultiInput<Types...>::registerUpdateNotifier(UpdateSlot_t* f)
+    {
+        std::vector<std::shared_ptr<Connection>> out_connection;
+        typeLoop<Types...>(*this, out_connection, f);
+        return std::make_shared<MultiConnection>(std::move(out_connection));
+    }
+
+    template <class... Types>
+    std::shared_ptr<Connection> TMultiInput<Types...>::registerUpdateNotifier(std::shared_ptr<TSignalRelay<UpdateSig_t>>& relay)
+    {
+        std::vector<std::shared_ptr<Connection>> out_connection;
+        typeLoop<Types...>(*this, out_connection, relay);
+        return std::make_shared<MultiConnection>(std::move(out_connection));
+    }
+
+    template <class... Types>
+    std::shared_ptr<Connection> TMultiInput<Types...>::registerUpdateNotifier(ISlot* f)
+    {
+        std::vector<std::shared_ptr<Connection>> out_connection;
+        typeLoop<Types...>(*this, out_connection, f);
+        return std::make_shared<MultiConnection>(std::move(out_connection));
+    }
+
+    template <class... Types>
+    std::shared_ptr<Connection> TMultiInput<Types...>::registerUpdateNotifier(std::shared_ptr<ISignalRelay> relay)
+    {
+        std::vector<std::shared_ptr<Connection>> out_connection;
+        typeLoop<Types...>(*this, out_connection, relay);
+        return std::make_shared<MultiConnection>(std::move(out_connection));
+    }
+
+    template <class... Types>
+    template<class T>
+    void TMultiInput<Types...>::apply(bool* modified) const
+    {
+        const auto val = mo::get<TInputParamPtr<T>>(m_inputs).modified();
+        if(val)
+        {
+            *modified = val;
+        }
+    }
+
+    template <class... Types>
+    template<class T>
+    void TMultiInput<Types...>::apply(bool modified, const ModifiedTag)
+    {
+        mo::get<TInputParamPtr<T>>(m_inputs).modified(modified);
+    }
+
+    template <class... Types>
+    bool TMultiInput<Types...>::modified() const
+    {
+        if(m_current_input)
+        {
+            return m_current_input->modified();
+        }
+        return false;
+    }
+
+    template <class... Types>
+    void TMultiInput<Types...>::modified(bool value)
+    {
+        typeLoop<Types...>(*this, value, ModifiedTag{});
     }
 
 } // namespace mo
