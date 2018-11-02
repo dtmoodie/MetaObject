141f498bb1ecc12a86485fec560755c8a0a59291
dan <dtmoodie@gmail.com>
2018-11-02 08:37:41 -0400

    wip improving multi input unit test

--- modules/params/tests/polymorphic_input.cpp
+++ modules/params/tests/polymorphic_input.cpp
@@ -7,57 +7,99 @@
 #include <boost/thread/recursive_mutex.hpp>
 #include <iostream>
 
-void printInputs(const std::tuple<const int*, const float*, const double*>& inputs)
+bool printInputs(const std::tuple<const int*, const float*, const double*>& inputs)
 {
     if (std::get<0>(inputs))
     {
         std::cout << "[int] " << *std::get<0>(inputs) << std::endl;
-        return;
+        return true;
     }
     if (std::get<1>(inputs))
     {
         std::cout << "[float] " << *std::get<0>(inputs) << std::endl;
-        return;
+        return true;
     }
     if (std::get<2>(inputs))
     {
         std::cout << "[double] " << *std::get<0>(inputs) << std::endl;
-        return;
+        return true;
     }
     std::cout << "No input set" << std::endl;
+    return false;
 }
-BOOST_AUTO_TEST_CASE(polymorphic_input)
+
+void clearInputs(std::tuple<const int*, const float*, const double*>& inputs)
 {
+    std::get<0>(inputs) = nullptr;
+    std::get<1>(inputs) = nullptr;
+    std::get<2>(inputs) = nullptr;
+}
+
+struct Fixture
+{
+    Fixture()
+    {
+        int_out.updatePtr(&int_val);
+        float_out.updatePtr(&float_val);
+        double_out.updatePtr(&double_val);
+
+        multi_input.setMtx(&mtx);
+        multi_input.setUserDataPtr(&inputs);
+    }
+
+    void checkInit()
+    {
+        BOOST_REQUIRE(multi_input.getInputParam() == nullptr);
+    }
+
+    void testInput(int val)
+    {
+        BOOST_REQUIRE(multi_input.setInput(&int_out));
+
+        BOOST_REQUIRE(multi_input.getInputParam());
+
+        BOOST_REQUIRE_EQUAL(multi_input.getInputParam(), &int_out);
+        int_out.updateData(6);
+    }
+
     std::tuple<const int*, const float*, const double*> inputs;
     int int_val;
     mo::TParamOutput<int> int_out;
-    int_out.updatePtr(&int_val);
 
     float float_val;
     mo::TParamOutput<float> float_out;
-    float_out.updatePtr(&float_val);
 
     double double_val;
     mo::TParamOutput<double> double_out;
-    double_out.updatePtr(&double_val);
 
-    printInputs(inputs);
     mo::TMultiInput<int, float, double> multi_input;
-    mo::Mutex_t mtx;
-    multi_input.setMtx(&mtx);
-    multi_input.setUserDataPtr(&inputs);
-    BOOST_REQUIRE(multi_input.setInput(&int_out));
 
-    BOOST_REQUIRE(multi_input.getInputParam());
-    BOOST_REQUIRE_EQUAL(multi_input.getInputParam(), &int_out);
-    int_out.updateData(6);
+    mo::Mutex_t mtx;
+};
+
+BOOST_FIXTURE_TEST_CASE(init, Fixture)
+{
+    checkInit();
+    BOOST_REQUIRE(!printInputs(inputs));
+}
+
+BOOST_AUTO_TEST_CASE(polymorphic_input)
+{
+
     BOOST_REQUIRE_NE(mo::get<const int*>(inputs), (void*)nullptr);
     BOOST_REQUIRE_EQUAL(*mo::get<const int*>(inputs), 6);
-    printInputs(inputs);
+
+    BOOST_REQUIRE(printInputs(inputs));
 
     int_out.updateData(5);
+    BOOST_REQUIRE_NE(mo::get<const int*>(inputs), (void*)nullptr);
+    BOOST_REQUIRE_EQUAL(*mo::get<const int*>(inputs), 5);
+
     auto data = multi_input.getData(mo::Header());
-    printInputs(inputs);
+    BOOST_REQUIRE(printInputs(inputs));
+
+    BOOST_REQUIRE(data);
+    BOOST_REQUIRE(data->getType() == mo::TypeInfo(typeid(int)));
 
     mo::TMultiOutput<int, float, double> multi_output;
 }
