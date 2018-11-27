9d7efaf9180984cb5ffcc7fc043fa19d9990afe5
dan <dtmoodie@gmail.com>
2018-11-27 00:03:27 -0500

    add a wait in here to simulate actually doing something

--- modules/core/examples/fiber.cpp
+++ modules/core/examples/fiber.cpp
@@ -15,12 +15,14 @@ int main()
     std::shared_ptr<mo::ThreadPool> pool = mo::sharedSingleton<mo::ThreadPool>(table.get());
     boost::fibers::use_scheduling_algorithm<mo::PriorityScheduler>(pool);
 
+    auto stream = mo::AsyncStreamFactory::instance(table.get())->create();
+
     for (uint32_t i = 0; i < 1000; ++i)
     {
-        boost::fibers::fiber f([]() {
-
+        stream->pushWork([]() {
+            // This is where we do all the things
+            boost::this_thread::sleep_for(boost::chrono::milliseconds(1));
         });
-        f.detach();
     }
 
     boost::this_fiber::yield();
