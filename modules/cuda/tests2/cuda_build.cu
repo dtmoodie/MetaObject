b66df0a4c16b09e2da506b0c340c53aba3f6dd41
dan <dtmoodie@gmail.com>
2018-12-15 15:36:11 -0500

    bare bones test for nvcc compat

---
+++ modules/cuda/tests2/cuda_build.cu
@@ -0,0 +1,10 @@
+#include <MetaObject/thread/fiber_include.hpp>
+#include <iostream>
+
+#include <boost/test/test_tools.hpp>
+#include <boost/test/auto_unit_test.hpp>
+
+int main()
+{
+    std::cout << __NVCC__ << " " << __GNUC__ << std::endl;
+}
