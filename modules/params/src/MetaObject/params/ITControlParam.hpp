751724ed53106d935bc39207e2ca6c3c5bbe016c
asiansensation <dtmoodie@gmail.com>
2020-07-06 02:10:10 +0000

    move comment

--- modules/params/src/MetaObject/params/ITControlParam.hpp
+++ modules/params/src/MetaObject/params/ITControlParam.hpp
@@ -11,12 +11,14 @@ namespace mo
         {
             this->setFlags(ParamFlags::kCONTROL);
         }
-        // THESE ARE NOT THREADSAFE.  Since it is expected that you may be trying to mutate a parameter
-        // in place, these put the onus on the user to lock for the duration of access
+
         TypeInfo getTypeInfo() const override
         {
             return TypeInfo::create<T>();
         }
+
+        // THESE ARE NOT THREADSAFE.  Since it is expected that you may be trying to mutate a parameter
+        // in place, these put the onus on the user to lock for the duration of access
         virtual T& getValue() = 0;
         virtual const T& getValue() const = 0;
         virtual void setValue(T&& val) = 0;
