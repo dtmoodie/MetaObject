cb4441f8dd68b3516f095d77193df6254a9e4113
asiansensation <dtmoodie@gmail.com>
2020-07-27 17:47:54 +0000

    implement SubscriberAny

--- modules/params/src/MetaObject/params/SubscriberAny.cpp
+++ modules/params/src/MetaObject/params/SubscriberAny.cpp
@@ -12,6 +12,20 @@ namespace mo
         this->appendFlags(ParamFlags::kINPUT);
     }
 
+    bool SubscriberAny::setInput(std::shared_ptr<IPublisher> param)
+    {
+        m_shared_publisher = param;
+        m_publisher = param.get();
+        return true;
+    }
+
+    bool SubscriberAny::setInput(IPublisher* param)
+    {
+        m_shared_publisher.reset();
+        m_publisher = param;
+        return true;
+    }
+
     bool SubscriberAny::acceptsPublisher(const IPublisher&) const
     {
         return true;
@@ -20,6 +34,79 @@ namespace mo
     bool SubscriberAny::acceptsType(const TypeInfo&) const
     {
         return true;
+    }
+
+    std::vector<TypeInfo> SubscriberAny::getInputTypes() const
+    {
+        return {};
+    }
+
+    bool SubscriberAny::isInputSet() const
+    {
+        return m_publisher != nullptr;
+    }
+
+    IDataContainerConstPtr_t SubscriberAny::getCurrentData(IAsyncStream* stream) const
+    {
+        IDataContainerConstPtr_t output;
+        if (m_publisher)
+        {
+            output = m_publisher->getData(nullptr, stream);
+        }
+        return output;
+    }
+
+    bool SubscriberAny::hasNewData() const
+    {
+        if (!m_publisher)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    boost::optional<Header> SubscriberAny::getNewestHeader() const
+    {
+        boost::optional<Header> output;
+        if (m_publisher)
+        {
+            output = m_publisher->getNewestHeader();
+        }
+        return output;
+    }
+
+    IDataContainerConstPtr_t SubscriberAny::getData(const Header* desired, IAsyncStream* stream)
+    {
+        IDataContainerConstPtr_t output;
+        if (m_publisher)
+        {
+            output = m_publisher->getData(desired, stream);
+        }
+        return output;
+    }
+
+    std::vector<Header> SubscriberAny::getAvailableHeaders() const
+    {
+        std::vector<Header> output;
+        if (m_publisher)
+        {
+            output = m_publisher->getAvailableHeaders();
+        }
+        return output;
+    }
+
+    std::ostream& SubscriberAny::print(std::ostream& os) const
+    {
+        if (m_publisher)
+        {
+            m_publisher->print(os);
+        }
+        return os;
+    }
+
+    IPublisher* SubscriberAny::getPublisher() const
+    {
+        return m_publisher;
     }
 
 } // namespace mo
--- modules/params/src/MetaObject/params/SubscriberAny.hpp
+++ modules/params/src/MetaObject/params/SubscriberAny.hpp
@@ -9,10 +9,30 @@ namespace mo
       public:
         SubscriberAny(const std::string& name = "");
 
+        bool setInput(std::shared_ptr<IPublisher> param) override;
+        bool setInput(IPublisher* param = nullptr) override;
+
         bool acceptsPublisher(const IPublisher& param) const override;
         bool acceptsType(const mo::TypeInfo& type) const override;
 
+        std::vector<TypeInfo> getInputTypes() const override;
+        bool isInputSet() const override;
+
+        IDataContainerConstPtr_t getCurrentData(IAsyncStream* = nullptr) const override;
+
+        bool hasNewData() const override;
+        boost::optional<Header> getNewestHeader() const override;
+
+        IDataContainerConstPtr_t getData(const Header* desired = nullptr, IAsyncStream* stream = nullptr) override;
+
+        std::vector<Header> getAvailableHeaders() const override;
+
+        std::ostream& print(std::ostream&) const override;
+        IPublisher* getPublisher() const override;
+
       protected:
+        IPublisher* m_publisher = nullptr;
+        std::shared_ptr<IPublisher> m_shared_publisher;
     };
 } // namespace mo
 
