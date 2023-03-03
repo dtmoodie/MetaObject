9e75722af6b6fc42d025003fb6021d26ab7d77fa
dan <dtmoodie@gmail.com>
2023-03-03 00:29:06 -0500

    find ct before rcc

--- cmake/metaobject_dependencies.cmake
+++ cmake/metaobject_dependencies.cmake
@@ -2,10 +2,10 @@ include(cmake/dependencies/boost.cmake)
 include(cmake/dependencies/cereal.cmake)
 include(cmake/dependencies/opencv.cmake)
 include(cmake/dependencies/cuda.cmake)
+add_subdirectory(dependencies/ct)
 include(cmake/dependencies/rcc.cmake)
 include(cmake/dependencies/spdlog.cmake)
 set(CT_HAVE_PYTHON 0 CACHE BOOL INTERNAL FORCE)
-add_subdirectory(dependencies/ct)
 include(dependencies/ct/cmake/python.cmake)
 
 
